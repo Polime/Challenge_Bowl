03be05cd8bd8c7c6cbf21f455e762a9105396442
Polime <530875962@qq.com>
2017-05-06 18:04:03 +0800

    update

--- index/back_end/conn.php
+++ index/back_end/conn.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+	session_start();
 	$pdo = new PDO("mysql:host=localhost;dbname=older","root","root");
 	$pdo->query("set names utf8;");
  ?>
---
+++ login/back_end/SendSms.php
@@ -0,0 +1,22 @@
+﻿
+<?php
+
+
+require 'Util.php';
+//发送验证码短信接口
+// $phone = "18758681219";
+// $code = "1111";
+$phone = $_GET['phone'];
+$code = $_GET['code'];
+$url = 'http://sms.jiangukj.com/SendSms.aspx?';
+$data = array('action' => 'code',
+    'username' => 'polime',
+    'userpass' => 'cch87219266',
+    'mobiles' => $phone,
+    'content' => $code,
+    'codeid' => '3396',);
+var_dump($data);
+$result = Util::request($url, 'Get',$data);
+echo $result;
+echo "<script>alert($result);</script>";
+?>
